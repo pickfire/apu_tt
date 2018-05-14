a93f369efb3b2b84b52d1bd8381ab2e9eb7532f7
Ivan Tham <pickfire@riseup.net>
2018-05-15 01:02:06 +0800

    refactor: use snake_case in struct

--- src/main.rs
+++ src/main.rs
@@ -17,27 +17,28 @@ use yansi::Paint;
 const URL: &str = "https://ws.apiit.edu.my/web-services/index.php/open/weektimetable";
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 struct Class {
-    INTAKE: String,
-    MODID: String,
-    DAY: String,
-    LOCATION: String,
-    ROOM: String,
-    LECTID: String,
-    DATESTAMP: String,
-    TIME_FROM: String,
-    TIME_TO: String,
+    intake: String,
+    modid: String,
+    day: String,
+    location: String,
+    room: String,
+    lectid: String,
+    datestamp: String,
+    time_from: String,
+    time_to: String,
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let data: Vec<Class> = reqwest::get(URL)?.json()?;
     let classes: Vec<_> = data.into_iter()
         .filter(|c| {
-            c.INTAKE == "UC2F1805CS(DA)"
-                && (c.MODID.contains("T-1") || c.MODID.contains("L") || c.MODID.contains("(LS)"))
+            c.intake == "UC2F1805CS(DA)"
+                && (c.modid.contains("T-1") || c.modid.contains("L") || c.modid.contains("(LS)"))
         })
         .map(|c| Class {
-            LOCATION: c.LOCATION.replace("NEW CAMPUS", "NEW"),
+            location: c.location.replace("NEW CAMPUS", "NEW"),
             ..c
         })
         .collect();
@@ -47,12 +48,12 @@ fn main() -> Result<(), Box<std::error::Error>> {
         writeln!(
             &mut tw,
             "{}\t{}\t{}\t{}\t{}\t{}",
-            Paint::purple(class.DAY),
-            Paint::green(format!("{}-{}", class.TIME_FROM, class.TIME_TO)),
-            Paint::blue(class.LOCATION).bold(),
-            Paint::red(class.ROOM),
-            Paint::yellow(class.MODID),
-            Paint::cyan(class.LECTID),
+            Paint::purple(class.day),
+            Paint::green(format!("{}-{}", class.time_from, class.time_to)),
+            Paint::blue(class.location).bold(),
+            Paint::red(class.room),
+            Paint::yellow(class.modid),
+            Paint::cyan(class.lectid),
         )?;
     }
     tw.flush()?;
