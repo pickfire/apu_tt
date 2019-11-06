d063dc377267bb0aa434b8f27d0ec4860b60e92f
Ivan Tham <pickfire@riseup.net>
2019-11-07 00:40:51 +0800

    chore: update classes

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 response
                     .json::<Vec<Class>>()?
                     .into_iter()
-                    .filter(|c| c.intake == "UC2F1805CS(DA)" && !c.modid.starts_with("MPU"))
+                    .filter(|c| c.intake == "UC3F1906CS(DA)")
                     .map(|c| Class {
                         location: c.location.trim_end_matches(" CAMPUS").to_owned(),
                         ..c
